e58ad3c5a714eb9191bd7086b3e9377328447ce8
talipcr <talip.cesur@gmail.com>
2021-05-20 16:20:57 +0200

    feat(common) add http-exception/error.middleware/not-found.middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { itemsRouter } from './items/items.router';
+import { errorHandler } from './middleware/error.middleware';
+import { notFoundHandler } from './middleware/not-found.middleware';
 
 dotenv.config();
 
@@ -23,6 +25,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use('/api/menu/items', itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 // Server Activation
 
